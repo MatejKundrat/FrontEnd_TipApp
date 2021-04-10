083d55939d1de1e169da4d75dd7e75aeb818b49c
MatejKundrat <74267171+MatejKundrat@users.noreply.github.com>
2021-04-10 21:12:28 +0200

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,15 +1,9 @@
-
-
-function calculateTip() {
+window.onload = function(){ 
+document.getElementById('calculate').onclick = function calculateTip() {
     var billAmt = document.getElementById("billamt").value;
     var serviceQual = document.getElementById("serviceQual").value;
     var numOfPeople = document.getElementById("peopleamt").value;
-    var total = (billAmt * serviceQual) / numOfPeople;
-    alert('Sprepitne je:' + total);
+    var total = (billAmt * (serviceQual / 100 )) / numOfPeople;
+    alert('Sprepitne je: ' + total + " Eur na osobu");
   };
-
-// function (){
-
-// document.getElementById("calculate").addEventListener("click", calculateTip());
-
-// }
+}
