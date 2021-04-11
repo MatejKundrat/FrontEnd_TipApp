e3ca2499e606a4aae5a52ffc30530534a5f887ff
MatejKundrat <74267171+MatejKundrat@users.noreply.github.com>
2021-04-11 21:30:20 +0200

    Fixnuta app.js - nastavene chybove hlasky, dopracovany style.css

--- script/app.js
+++ script/app.js
@@ -4,6 +4,34 @@ document.getElementById('calculate').onclick = function calculateTip() {
     var serviceQual = document.getElementById("serviceQual").value;
     var numOfPeople = document.getElementById("peopleamt").value;
     var total = (billAmt * (serviceQual / 100 )) / numOfPeople;
-    alert('Sprepitne je: ' + total + " Eur na osobu");
+    if (billAmt === '') {
+      alert('Nevyplnil si vsetky policka')
+      return;
+    }
+    if (serviceQual === '') {
+      alert('Nevyplnil si vsetky policka')
+      return;
+    }
+    if (numOfPeople === '') {
+      alert('Nevyplnil si vsetky policka')
+      return;
+    }
+    if (numOfPeople === '0') {
+      alert('Nemohlo vas byt 0 :) ')
+      return;
+    }
+    if (numOfPeople < 0) {
+      alert('Napisal si zaporne cislo, skus to znovu a pouzi len kladne cisla :)')
+      return;
+    }
+    if (billAmt < 0) {
+      alert('Napisal si zaporne cislo, skus to znovu a pouzi len kladne cisla :)')
+      return;
+    }
+    if (serviceQual < 0) {
+      alert('Napisal si zaporne cislo, skus to znovu a pouzi len kladne cisla :)')
+      return;
+    }
+    alert('Kazdy z vas zaplati ' + total + " Eur na sprepitne");
   };
 }
